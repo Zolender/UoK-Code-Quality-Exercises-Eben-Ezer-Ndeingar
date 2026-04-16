47901da64a0010c6e757eac1d8ba4945702af11f
Zolender <ndeingare@gmail.com>
2026-04-16 22:51:04 +0200

    improve original discount function, changing name and syntax

---
+++ discount.js
@@ -0,0 +1,11 @@
+// function discount(totalamount) {
+//   var disc;
+//   if (totalamount > 100) {
+//     disc = totalamount * 0.1;
+//   } else {
+//     disc = totalamount * 0.05;
+//   }
+//   return disc;
+// }
+
+const discountRate = (totalAmount)=> totalAmount > 100 ? totalAmount * 0.1 : totalAmount * 0.05
