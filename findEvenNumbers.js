865bc5c09213e1284cf9ee42902e734da537198e
Zolender <ndeingare@gmail.com>
2026-04-16 15:46:37 +0200

    improves the findEvenNumbers function logic

---
+++ findEvenNumbers.js
@@ -0,0 +1,10 @@
+function findEvenNumbers(numbers) {
+//   var evenNumbers = [];
+//   for (var i = 0; i < numbers.length; i++) {
+//     if (numbers[i] % 2 === 0) {
+//       evenNumbers.push(numbers[i]);
+//     }
+//   }
+//   return evenNumbers;
+    return numbers.filter(num=> num % 2===0)
+}
