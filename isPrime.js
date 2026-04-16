472284a5b3c8fe0e0d2bb64d6534c3966b529c87
Zolender <ndeingare@gmail.com>
2026-04-16 23:09:01 +0200

    implement a function to check if a number is prime

---
+++ isPrime.js
@@ -0,0 +1,10 @@
+const isPrime = (n)=>{
+    if(n<=0)return false
+    if(n<=3)return true
+    if(n%2===0)return false
+
+    for(let i=5; i <= Math.sqrt(n); i+=2){
+        if(n%i===0)return false
+    }
+    return true
+}
