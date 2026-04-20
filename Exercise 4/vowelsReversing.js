04aff6ab0893c652bc84bbf4fce48f0bbaa7d22c
Zolender <ndeingare@gmail.com>
2026-04-21 01:08:46 +0200

    refactor reverseVowels function for simplicity and efficiency

--- Exercise 4/vowelsReversing.js
+++ Exercise 4/vowelsReversing.js
@@ -1,21 +1,4 @@
 function reverseVowels(str) {
-    if(!str.trim())return str
-    
-    const vowels = new Set(["e","o","u","i","a"])
-    const order = []
-    const temp = str.split("").map(x => {
-                                    if(vowels.has(x.toLowerCase())){
-                                        order.push(x)
-                                        return "^"
-                                    }
-                                    return x
-                                    } 
-                                )
-    const result = temp.map((y)=>{
-        if(y==="^"){
-        return order.pop()
-        }
-        return y
-    })
-    return result.join("")
+    const vowels = str.replace(/[^aeuio]/gi, "").split("")
+    return str.replace(/[aeuio]/gi, (_)=> vowels.pop())
 }
